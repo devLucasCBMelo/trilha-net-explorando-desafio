588ba75f9e954be87191c6b836ce7457112717b1
Lucas Costa <englucascbmelo@gmail.com>
2024-09-25 21:53:10 -0300

    subindo o método CadastrarHospedes e o CalcularValorDiaria

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,9 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            bool check = Suite.Capacidade > hospedes.Count ? true : false;
+
+            if (check)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +27,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new ArgumentException("Capacidade do quarto insuficiente");
             }
         }
 
@@ -44,13 +47,15 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            Console.WriteLine($"meu valor: {valor}");
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.1M * valor;
+                valor = valor - desconto;
             }
 
             return valor;
